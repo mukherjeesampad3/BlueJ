c05d3f0bb12eb5fe1e82cc64ac5313feb089c01b
Sampad Mukherjee <49312920+mukherjeesampad3@users.noreply.github.com>
2022-06-18 18:05:36 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class add here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class add
+{
+   public static void main(String[]args){
+    int a,b,c;
+    a=10;
+    b=15;
+    c=a+b;
+    System.out.println(c);
+    
+    }
+}
---
+++ swap.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class swap here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class swap
+{
+   public static void main(String args[]){
+    int a=10;
+    int b=20;
+    System.out.println("Values before swapping a : " +a+" b : "+b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    System.out.println("Values after swapping a : " +a+" b : "+b);
+    
+    
+    }
+}
