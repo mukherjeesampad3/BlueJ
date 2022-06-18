145a7d6a6904eeb0f0f4694b8617b1bb3db195e6
Sampad Mukherjee <49312920+mukherjeesampad3@users.noreply.github.com>
2022-06-18 18:04:16 +0530

    Commit
    First Program to BlueJ - Hello World

---
+++ ex1.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class ex1 here.
+ *
+ * @author (Sampad)
+ * @version (a version number or a date)
+ */
+public class ex1
+{
+   public static void main(String[]args){
+        System.out.println("Hello World !!");
+    }
+}
